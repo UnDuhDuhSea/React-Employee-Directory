dc126e410dcfd8a6e4a34f41145dc1f2a17b6fed
UnDuhDuhSea <tafimba@gmail.com>
2021-05-14 15:37:21 -0700

    Adding API functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
+import React, { Component } from "react";
 import Jumbotron from "./components/Jumbotron";
 import SearchInput from "./components/SearchInput";
 import EmployeeTable from "./components/EmployeeTable";
+import API from "./utils/API";
 
-function App() {
-  return (
-    <div>
-      <Jumbotron />
-      <SearchInput />
-      <EmployeeTable />
-    </div>
-  );
+class App extends Component {
+  state = {
+    search: "",
+    employees: [],
+  };
+
+  componentDidMount() {
+    this.fetchAllEmployees();
+  }
+
+  fetchAllEmployees = () => {
+    API.getUsers()
+      .then((res) => this.setState({ employees: res.data.results }))
+      .catch((err) => console.log(err));
+  };
+
+  handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <Jumbotron />
+        <SearchInput />
+        <EmployeeTable />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,17 +1,22 @@
 import React from "react";
 
-function SearchInput() {
+function SearchInput(props) {
   return (
     <div class="p-5 col-lg-3 mx-auto input-group input-group-lg">
-      <div class="input-group-prepend">
-        <span
-          class="input-group-text bg-primary text-light"
-          id="inputGroup-sizing-lg"
+      <div type="button" class="input-group-prepend">
+        <button
+          onClick={props.handleFormSubmit}
+          class="btn btn-outline-secondary"
+          type="button"
+          id="button-addon1"
         >
-          Submit
-        </span>
+          Button
+        </button>
       </div>
       <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="search"
         type="text"
         class="form-control"
         aria-label="Sizing example input"
